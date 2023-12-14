173e5507f53491f4d4181af49e25ade3c1903e77
clement-caillat <dev.clement-caillat@proton.me>
2023-12-14 15:41:08 +0100

    Big updates

--- src/core/database.rs
+++ src/core/database.rs
@@ -19,6 +19,13 @@ impl WalletDatabase {
         })
     }
 
+    pub fn get_address(&self) -> Result<String, Error> {
+        let result = self.database.get(b"address")?;
+        let address = result.unwrap();
+        let address = std::str::from_utf8(address.as_ref()).unwrap();
+        Ok(String::from(address))
+    }
+
     pub fn close(self) -> Result<(), Error>{
         self.database.flush()?;
         drop(self.database);
@@ -48,5 +55,29 @@ impl WalletDatabase {
     #[cfg(test)]
     pub fn delete(path: &str) {
         std::fs::remove_dir_all(path).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_store_address() {
+        let db = WalletDatabase::open("F:/Dev/Web3/libs/spark_core/test_storage").unwrap();
+        let result = db.store_address(String::from("1c58uBkR6arPznA7XHiyhjH9ScYDsise8mTWKa2"));
+        assert!(result.is_ok());
+        drop(result);
+
+        let result = db.get_address();
+        assert!(result.is_ok());
+        let address = result.unwrap();
+
+        assert_eq!(address, "1c58uBkR6arPznA7XHiyhjH9ScYDsise8mTWKa2");
+
+        db.close().unwrap();
+
+        WalletDatabase::delete("F:/Dev/Web3/libs/spark_core/test_storage");
+
     }
 }
--- src/core/wallet.rs
+++ src/core/wallet.rs
@@ -15,29 +15,50 @@ use super::address::Address;
 
 
 pub struct Wallet {
-    private_key: [u8; 32],
-    address: String,
+    db: WalletDatabase,
+    pub address: String,
 }
 
 
 impl Wallet {
-    pub fn init(path: &str, password: &str) -> Result<String, Error> {
+    pub fn init(path: &str, password: &str) -> Result<Wallet, Error> {
         <Wallet as Initiator>::init(path, password)
+    }
+
+    pub fn open(path: &str) -> Result<Wallet, Error> {
+        <Wallet as Opener>::open(path)
     }
 }
 
+trait Opener {
+    fn open(path: &str) -> Result<Wallet, Error>;
+}
+
+impl Opener for Wallet {
+    fn open(path: &str) -> Result<Wallet, Error> {
+        let db: WalletDatabase = WalletDatabase::open(path)?;
+        let address: String = db.get_address()?;
+
+        Ok(Wallet {
+            db,
+            address
+        })
+    }
+
+}
+
 trait Initiator {
-    fn init(path: &str, password: &str) -> Result<String, Error>;
-    fn create_wallet() -> Wallet;
+    fn init(path: &str, password: &str) -> Result<Wallet, Error>;
+    fn create_key_pair() -> ([u8; 32], String);
     fn get_encryption_key(salt: [u8; 16], password: &str, password_hash: &str) -> Result<[u8; 32], Error>;
     fn get_password_hash(password: &str) -> Result<String, Error>;
 }
 
 impl Initiator for Wallet {
-    fn init(path: &str, password: &str) -> Result<String, Error> {
+    fn init(path: &str, password: &str) -> Result<Self, Error> {
         let db: WalletDatabase = WalletDatabase::open(path)?;
         
-        let wallet: Wallet = Self::create_wallet();
+        let keypair: ([u8; 32], String) = Self::create_key_pair();
         
         let password_hash: String = Self::get_password_hash(password)?;
 
@@ -45,25 +66,26 @@ impl Initiator for Wallet {
 
         let encryption_key: [u8; 32] = Self::get_encryption_key(salt, password, &password_hash)?;
         
-        let encrypted_private_key = crypto::encrypt_data(encryption_key, &wallet.private_key)?;
+        let encrypted_private_key = crypto::encrypt_data(encryption_key, &keypair.0)?;
         
         
-        let address = wallet.address.clone();    
+        let address = keypair.1.clone();
         
         db.store_salt(salt)?;
         
         db.store_private_key(encrypted_private_key)?;
         
-        db.store_address(wallet.address)?;
+        db.store_address(keypair.1)?;
 
         db.store_password(password_hash)?;
 
-        db.close()?;
-
-        Ok(address)
+        Ok(Wallet {
+            db,
+            address
+        })
     }
 
-    fn create_wallet() -> Wallet {
+    fn create_key_pair() -> ([u8; 32], String) {
         let mut rng: OsRng = OsRng;
 
         let private_key = SigningKey::random(&mut rng);
@@ -74,10 +96,7 @@ impl Initiator for Wallet {
         
         let address: String = Address::new(public_key.as_bytes());
 
-        Wallet {
-            private_key,
-            address
-        }
+        (private_key, address)
     }
 
     fn get_password_hash(password: &str) -> Result<String, Error>{
